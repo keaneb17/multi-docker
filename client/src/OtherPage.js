46f809563fabf22bfd995605a5d05c6a5540908d
keaneb17 <bkeane@novogrid.com>
2020-07-06 14:17:31 +0100

    apptest file corrected

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default () => {
   return (
     <div>
-      <p>Im some other page</p>
+      Im some other page
       <Link to="/">Go back to home page!</Link>
     </div>
   );
